f3bd6c98914a4b3c6223fc7df35288648078226f
Gabriel Licar <114968559+GabrielLicar@users.noreply.github.com>
2022-12-30 17:14:50 -0300

    Changes Significativas
    Co-authored-by: João Albéris <joaoalberis@users.noreply.github.com>

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,17 @@
 async function generateTextJujutsu() {
+
   // Pegue o texto dos inputs
   let typequest = document.getElementById("typequest").value;
   let typereward = document.getElementById("typereward").value;
   let caracdialogue = document.getElementById("caracdialogue").value;
   let location = document.getElementById("location").value;
 
+  let loading = document.getElementById("resultado");
+  loading.innerHTML = "Carregando...";
+
   // Faça uma chamada à API GPT-3 usando o método completions
   const prompt = `local da missao: ${location}\n quero que faça um roteiro para o anime Jujutsu, com os seguintes topicos e utilizando de base o local da missao citado: \nDialogo: nesse topico quero que faça um dialogo para o jogador ler antes de começar a quest, quero algo nem grande, mas tambem nao pequeno quero que tenha um dialogo com os personagens ${caracdialogue} \ntipo da recompensa: ${typereward}(escolha uma das opções) \nrecompensa: escolha uma das recompensas do topico tipo da recompensa e sete uma quantidade \ntipo da quest: ${typequest} escolha uma das opções do topico "tipo da quest" e selecione oque sera feito na missao.`;
-  const apiKey = "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";
+  const apiKey = "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";
   const endpoint = "https://api.openai.com/v1/completions";
 
   const response = await fetch(endpoint, {
@@ -26,56 +30,111 @@ async function generateTextJujutsu() {
     })
   });
   const text = await response.json()
-  document.getElementById("resposta").innerHTML = `<h1>${text.choices[0].text}</h1>`
+  loading.innerHTML = "Resultado:";
+  document.getElementById("resposta").innerHTML = `${text.choices[0].text.replace(/\n/g, "<br>")}`
 }
 
-function generateTextNanatsu() {
-    // Pegue o texto do input
-    let prompt = document.getElementById("prompt-input").value;
+async function generateTextNanatsu() {
+    // Pegue o texto dos inputs
+  let typequest = document.getElementById("typequest").value;
+  let typereward = document.getElementById("typereward").value;
+  let caracdialogue = document.getElementById("caracdialogue").value;
+  let location = document.getElementById("location").value;
 
-    // Faça uma chamada à API GPT-3 usando o método completions
-    openai.completions({
-        engine: "text-davinci-002",
-        prompt: prompt,
-        max_tokens: 2048,
-        n: 1,
-        stop: "."
-    }, function(error, response) {
-        // Imprima o texto gerado
-        console.log(response.choices[0].text);
-    });
+  let loading = document.getElementById("resultado");
+  loading.innerHTML = "Carregando...";
+
+  // Faça uma chamada à API GPT-3 usando o método completions
+  const prompt = `local da missao: ${location}\n quero que faça um roteiro para o anime Nanatsu, com os seguintes topicos e utilizando de base o local da missao citado: \nDialogo: nesse topico quero que faça um dialogo para o jogador ler antes de começar a quest, quero algo nem grande, mas tambem nao pequeno quero que tenha um dialogo com os personagens ${caracdialogue} \ntipo da recompensa: ${typereward}(escolha uma das opções) \nrecompensa: escolha uma das recompensas do topico tipo da recompensa e sete uma quantidade \ntipo da quest: ${typequest} escolha uma das opções do topico "tipo da quest" e selecione oque sera feito na missao.`;
+  const apiKey = "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";
+  const endpoint = "https://api.openai.com/v1/completions";
+
+  const response = await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+      prompt: prompt,
+      max_tokens: 2048,
+      n: 1,
+      stop: "§",
+      temperature: 0.5,
+      model: "text-davinci-003"
+    })
+  });
+  const text = await response.json()
+  loading.innerHTML = "Resultado:";
+  document.getElementById("resposta").innerHTML = `${text.choices[0].text.replace(/\n/g, "<br>")}`
 }
 
-function generateTextNaruto() {
-    // Pegue o texto do input
-    let prompt = document.getElementById("prompt-input").value;
+async function generateTextNaruto() {
+    // Pegue o texto dos inputs
+  let typequest = document.getElementById("typequest").value;
+  let typereward = document.getElementById("typereward").value;
+  let caracdialogue = document.getElementById("caracdialogue").value;
+  let location = document.getElementById("location").value;
 
-    // Faça uma chamada à API GPT-3 usando o método completions
-    openai.completions({
-        engine: "text-davinci-002",
-        prompt: prompt,
-        max_tokens: 2048,
-        n: 1,
-        stop: "."
-    }, function(error, response) {
-        // Imprima o texto gerado
-        console.log(response.choices[0].text);
-    });
+  let loading = document.getElementById("resultado");
+  loading.innerHTML = "Carregando...";
+
+  // Faça uma chamada à API GPT-3 usando o método completions
+  const prompt = `local da missao: ${location}\n quero que faça um roteiro para o anime Naruto, com os seguintes topicos e utilizando de base o local da missao citado: \nDialogo: nesse topico quero que faça um dialogo para o jogador ler antes de começar a quest, quero algo nem grande, mas tambem nao pequeno quero que tenha um dialogo com os personagens ${caracdialogue} \ntipo da recompensa: ${typereward}(escolha uma das opções) \nrecompensa: escolha uma das recompensas do topico tipo da recompensa e sete uma quantidade \ntipo da quest: ${typequest} escolha uma das opções do topico "tipo da quest" e selecione oque sera feito na missao.`;
+  const apiKey = "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";
+  const endpoint = "https://api.openai.com/v1/completions";
+
+  const response = await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+      prompt: prompt,
+      max_tokens: 2048,
+      n: 1,
+      stop: "§",
+      temperature: 0.5,
+      model: "text-davinci-003"
+    })
+  });
+  const text = await response.json()
+  loading.innerHTML = "Resultado:";
+  document.getElementById("resposta").innerHTML = `${text.choices[0].text.replace(/\n/g, "<br>")}`
 }
 
-function generateTextSoloLeveling() {
-    // Pegue o texto do input
-    let prompt = document.getElementById("prompt-input").value;
+async function generateTextSoloLeveling() {
+    // Pegue o texto dos inputs
+  let typequest = document.getElementById("typequest").value;
+  let typereward = document.getElementById("typereward").value;
+  let caracdialogue = document.getElementById("caracdialogue").value;
+  let location = document.getElementById("location").value;
 
-    // Faça uma chamada à API GPT-3 usando o método completions
-    openai.completions({
-        engine: "text-davinci-002",
-        prompt: prompt,
-        max_tokens: 2048,
-        n: 1,
-        stop: "."
-    }, function(error, response) {
-        // Imprima o texto gerado
-        console.log(response.choices[0].text);
-    });
+  let loading = document.getElementById("resultado");
+  loading.innerHTML = "Carregando...";
+
+  // Faça uma chamada à API GPT-3 usando o método completions
+  const prompt = `local da missao: ${location}\n quero que faça um roteiro para o manga Solo Leveling, com os seguintes topicos e utilizando de base o local da missao citado: \nDialogo: nesse topico quero que faça um dialogo para o jogador ler antes de começar a quest, quero algo nem grande, mas tambem nao pequeno quero que tenha um dialogo com os personagens ${caracdialogue} \ntipo da recompensa: ${typereward}(escolha uma das opções) \nrecompensa: escolha uma das recompensas do topico tipo da recompensa e sete uma quantidade \ntipo da quest: ${typequest} escolha uma das opções do topico "tipo da quest" e selecione oque sera feito na missao.`;
+  const apiKey = "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";
+  const endpoint = "https://api.openai.com/v1/completions";
+
+  const response = await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+      prompt: prompt,
+      max_tokens: 2048,
+      n: 1,
+      stop: "§",
+      temperature: 0.5,
+      model: "text-davinci-003"
+    })
+  });
+  const text = await response.json()
+  loading.innerHTML = "Resultado:";
+  document.getElementById("resposta").innerHTML = `${text.choices[0].text.replace(/\n/g, "<br>")}`
 }
